cd3cdbb5239cefcec28e0200e2e0fe3d5a0adf3d
asmckeithan <annesha.mckeithan@gmail.com>
2024-04-13 19:52:55 -0400

    Fizz Buzz

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ console.log("Fizz divisible by 5=================")
         console.log(`Buzz:${a}`)
     }
  }
-//  console.log("Fizz Buzz divisible by 3 and 5=================")
-//  for (let c = 0; c <= 100; c++){
-//     if (c % 3 ==0 && c % 5 ==0){
-//         console.log(`Fizz Buzz:${c}`)
-//     }
-//  }
+ console.log("Fizz Buzz divisible by 3 and 5=================")
+ for (let c = 0; c <= 100; c++){
+    if (c % 3 ==0 && c % 5 ==0){
+        console.log(`Fizz Buzz:${c}`)
+    }
+ }
 //  console.log("! Not divisible by 3 and 5=================")
 //  for (let d = 0; d <= 100; d++){
 //     if (d % 3 !==0 && d % 5 !==0){
