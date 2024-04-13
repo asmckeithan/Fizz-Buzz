cdd71b02df87bfa27caa0254c047b74d59698189
asmckeithan <annesha.mckeithan@gmail.com>
2024-04-13 19:49:09 -0400

    Loop to 100

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(`Fizz Buzz Activated `)
+console.log(`Part 1: =====================`)
+console.log("Loop through all number from 1 -100")
+
+for( b = 0; b <= 100; b++){
+    console.log(b)
+}
+
+// console.log("Fizz divisible by 3=================")
+// for (let i = 0; i <= 100; i++){
+//     if (i % 3 === 0){
+//       console.log(`Fizz:${i}`)
+//     }}
+
+// console.log("Fizz divisible by 3=================")
+//  for (let a = 0; a <= 100; a++){
+//     if (a % 5 === 0){
+//         console.log(`Buzz:${a}`)
+//     }
+//  }
+//  console.log("Fizz Buzz divisible by 3 and 5=================")
+//  for (let c = 0; c <= 100; c++){
+//     if (c % 3 ==0 && c % 5 ==0){
+//         console.log(`Fizz Buzz:${c}`)
+//     }
+//  }
+//  console.log("! Not divisible by 3 and 5=================")
+//  for (let d = 0; d <= 100; d++){
+//     if (d % 3 !==0 && d % 5 !==0){
+//         console.log(`!${d}`)
+//     }
+//  }
